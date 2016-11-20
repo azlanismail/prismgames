8ff93fc419771820c6d739579e59c7d7aad6e868
azlan <azlan@localhost.localdomain>
2016-11-20 21:10:33 +0000

    updated 20/11

--- src/planner/ModelGenerator.java
+++ src/planner/ModelGenerator.java
@@ -20,7 +20,7 @@ public class ModelGenerator {
 		pw = new PrintWriter(outfile);
 	}
 	
-	public void generateSingleGameModel()  {
+	public void generateSingleNodeModel()  {
 
    	 	String p1="p1", p2="p2";
    	 	String mod1="planner", mod2="environment";
@@ -291,20 +291,154 @@ public class ModelGenerator {
 	 	pw.close();
     }
 
+	public void generateSeqModel()  {
+
+   	 	String p1="p1", p2="p2";
+   	 	String mod1="planner", mod2="environment";
+   	 	int nodeNum = 2;	//number of activity/task/node in the service composition
+   	 	int maxActionP1 =10, maxActionP2 =5;
+   	 	
+   	 	pw.println("smg");
+   	 	pw.println("//=========Player definition=======");
+   	 	pw.println("player "+p1);
+   	 	pw.print(""+mod1+",");
+   	 	for(int n=0; n < nodeNum; n++) {
+   	 		for(int i=0; i < maxActionP1; i++) {
+   	 			pw.print("[n"+n+"r"+i+"]");
+   	 			if ((n !=(nodeNum-1)) && (i != (maxActionP1-1))) {
+   	 			pw.print(",");
+   	 			}
+   	 		}
+		}
+   	 	pw.println();
+   	 	pw.println("endplayer");
+   	 	pw.println();
+   	 	
+   	 	pw.println("player "+p2);
+   	 	pw.print(""+mod2+",");
+   	 	for(int n=0; n < nodeNum; n++) {
+   	 		for(int i=0; i < maxActionP2; i++) {
+   	 			pw.print("[n"+n+"r"+i+"]");
+   	 			if ((n !=(nodeNum-2)) && (i != (maxActionP2-1))) {
+   	 			pw.print(",");
+   	 			}
+   	 		}
+		}
+   	 	pw.println();
+   	 	pw.println("endplayer");
+   	 	pw.println();
+	 	
+   		
+	 	pw.println("//=========User Requirements=======");
+	 	pw.println("const int A0_ID = 0;");
+	 	pw.println("const int A0_DUR = 5;	//max duration");
+	 	pw.println("const double A0_REL = 0.6; //reliability");
+	 	pw.println("const double A0_COST = 10.0; //max cost");
+	 	pw.println("const double A0_WG_COST = 0.3; //weight for cost");
+	 	pw.println("const double A0_WG_DUR = 0.3; //weight for duration");
+	 	pw.println("const double A0_WG_REL = 0.4; //weight for reliability");
+	 	pw.println();
+	 	
+	 	pw.println("//=========Resource Profiles=======");
+	 	pw.println("cost int MXN="+nodeNum+";");
+	 	for(int n=0; n < nodeNum; n++) {
+	 		pw.println("const int N"+n+"_MAX_SV="+maxActionP1+";	//finite number of services");		
+	 		pw.println("const int N"+n+"_MAX_EV="+maxActionP2+";	//finite number of computing nodes");		
+	 	}
+	 	pw.println();
+	 	
+	 	for (int n=0; n < nodeNum; n++) {
+		 	for(int i=0; i < maxActionP1; i++) {
+		 		pw.println("const int N"+n+"_RS"+i+"_ID;");
+		 		pw.println("const double N"+n+"_RS"+i+"_COST;	//cost");
+		 		pw.println("const bool N"+n+"_RS"+i+"_AVAIL;	//availability status");
+		 		for(int j=0; j < maxActionP2; j++) {
+		 			pw.println("const int N"+n+"_RS"+i+"_DUR"+j+";	//duration "+j+";");
+		 		}
+		 		for(int j=0; j < maxActionP2; j++) {
+		 			pw.println("const int N"+n+"_RS"+i+"_REL"+j+";	//reliability "+j+";");
+		 		}
+		 		pw.println();
+		 	}
+	 	}
+	 	pw.println();
+   	 	
+		pw.println("//=========Global Parameters=======");
+	 	pw.println("const int TE=0;	//plater 2 state");	
+	 	pw.println("const int TP=1;	//player 1 state");
+		pw.println("const int TS=2;	//coordinator state");
+	 	pw.println("global t:[TE..TS] init TS;	//to control the turn");
+	 	pw.println("global end : bool init false;	//(absorbing state)");		
+	 	pw.println("global n:[0..MXN-1] init 0;  //to control the sequence");
+	 	pw.println();		
+	
+
+		pw.println("//=========Module for Player 1=======");
+	 	pw.println("module "+mod1);
+	 	for(int n=0; n < nodeNum; n++) {
+	 		pw.println("n"+n+":[-1..N"+n+"_MAX_SV-1] init -1;");
+	 	}
+	 	
+	 	pw.println("//P1 moves :");
+	 	pw.println("[] (t=TS) & (n < MXN) -> (t'=TP);");
+	 	pw.println("[] (t=TS) & (n >= MXN) -> (end'=true);");
+	 	for(int n=0; n < nodeNum; n++) {
+		 	for(int i=0; i < maxActionP1; i++) {
+		 		pw.println("[n"+n+"r"+i+"] (t=TP) & (n="+n+") & (a0_n"+n+"_rs"+i+"_sat_all=true) -> (n"+n+"'="+i+") & (t'=TE);");
+		 	}
+	 	}
+	 	pw.println("endmodule");
+	 	pw.println();
+	 	
+	 	pw.println("//=========Module for Player 2=======");
+	 	pw.println("module "+mod2);
+	 	for(int n=0; n < nodeNum; n++) {
+	 		pw.println("n"+n+"ev:[-1..N"+n+"_MAX_EV] init -1;");
+	 	}
+	 	pw.println("//P2 moves :");	
+	 	for(int n=0; n < nodeNum; n++) {
+		 	for(int i=0; i < maxActionP2; i++) {
+		 		pw.println("[n"+n+"e"+i+"] (t=TE) & (n="+n+") -> n"+n+"ev"+i+"_rel:(n"+n+"ev'="+i+") & (t'=TS) & (n'=n+1) + 1-n"+n+"ev"+i+"_rel:(n"+n+"ev'=-1) & (t'=TS);");			
+		 	}
+	 	}
+	 	pw.println("endmodule");
+	 	pw.println();
+	 	
+	 	
+	 	pw.close();
+    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String path = "/home/azlan/git/PrismGames/Prismfiles/mymodel.prism";
+		String pathSingle = "/home/azlan/git/PrismGames/Prismfiles/singlemodel.prism";
+		String pathSeq = "/home/azlan/git/PrismGames/Prismfiles/seqmodel.prism";
 		
+		int pattern = 1;
 		
-		try {
-			ModelGenerator mdg = new ModelGenerator(path);
-			mdg.generateSingleGameModel();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (pattern==0) {
+			try {
+				ModelGenerator mdg = new ModelGenerator(pathSingle);
+				mdg.generateSingleNodeModel();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		else if (pattern==1) {
+			try {
+				ModelGenerator mdg = new ModelGenerator(pathSeq);
+				mdg.generateSeqModel();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else
+			System.out.println("no pattern has been selected");
+			
+		
+		
+		
 		System.out.println("Model has been generated...");
 		
 		//StochasticPlanner sp = new StochasticPlanner();
